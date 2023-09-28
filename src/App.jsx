1a90ef44216849833e688b5e5968260f6ebb4eb5
Lenka Yaswith Krishna Vamsi <yaswith.lykv@gmail.com>
2023-09-29 01:55:47 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import './index.css'
 import { Home } from './components/Home'
 import { GamePage } from './components/GamePage'
+import {BrowserRouter} from 'react-router-dom'
 
 function App() {
   const [isGamePlaying, setIsGamePlaying] = useState(false);
   return (
     <>
+    <BrowserRouter>
     {
       isGamePlaying ? <GamePage/>: <Home isGamePlaying = {isGamePlaying} setIsGamePlaying = {setIsGamePlaying}/>
     }
+    </BrowserRouter>
     </>
   )
 }
